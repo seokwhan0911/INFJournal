d6729453dc801637efb1f9bbdd61242f3e0e601f
eomgerm <fishbread00@gmail.com>
2024-01-03 01:55:12 +0900

    :sparkles: [Feat]: 오늘의 주제 API 구현

--- src/main/java/com/um5th/hackerthon/infjournal/repository/EssayRepository.java
+++ src/main/java/com/um5th/hackerthon/infjournal/repository/EssayRepository.java
@@ -1,7 +1,11 @@
 package com.um5th.hackerthon.infjournal.repository;
 
 import com.um5th.hackerthon.infjournal.domain.Essay;
+import com.um5th.hackerthon.infjournal.domain.Member;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EssayRepository extends JpaRepository<Essay, Long> {
+
+    List<Essay> findAllByMember(Member member);
 }
