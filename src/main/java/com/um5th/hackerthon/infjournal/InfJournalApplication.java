17a9ce9092f4e0af4fff40a50369c5533072dd4f
eomgerm <fishbread00@gmail.com>
2024-01-02 14:38:00 +0900

    :rocket: Init Project

---
+++ src/main/java/com/um5th/hackerthon/infjournal/InfJournalApplication.java
@@ -0,0 +1,13 @@
+package com.um5th.hackerthon.infjournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfJournalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfJournalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/um5th/hackerthon/infjournal/InfJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.um5th.hackerthon.infjournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfJournalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
