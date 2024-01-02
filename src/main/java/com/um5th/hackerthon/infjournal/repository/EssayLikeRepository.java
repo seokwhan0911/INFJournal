e6cbb705a4d3d3127e6959d5ae10ff5b17cba231
ziy00n <jiyun4188@gmail.com>
2024-01-03 04:30:11 +0900

    [feat] 에세이 좋아요 카운트 임시저장- 추후 추가

--- src/main/java/com/um5th/hackerthon/infjournal/repository/EssayLikeRepository.java
+++ src/main/java/com/um5th/hackerthon/infjournal/repository/EssayLikeRepository.java
@@ -4,6 +4,8 @@ import com.um5th.hackerthon.infjournal.domain.Essay;
 import com.um5th.hackerthon.infjournal.domain.Member;
 import com.um5th.hackerthon.infjournal.domain.mapping.EssayLike;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
@@ -13,4 +15,8 @@ public interface EssayLikeRepository extends JpaRepository<EssayLike, Long> {
     Optional<EssayLike> findByMemberAndEssay(Member member, Essay essay);
 
     void deleteByMemberAndEssay(Member member, Essay essay);
+
+    //Integer countByEssay(Long essayId);
+    @Query("SELECT COUNT(e) FROM EssayLike e WHERE e.essay = :essay")
+    Integer countByEssay(@Param("essay") Essay essay);
 }
